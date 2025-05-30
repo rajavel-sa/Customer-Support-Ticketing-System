ff4d94f04a3f41ec003e1e84fbf036a17f710136
Rajavel <76117631+rajavel-sa@users.noreply.github.com>
2025-05-30 10:25:26 +0530

    Delete CSTS/src directory

--- CSTS/src/main/java/com/Eval_Task/CSTS/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Eval_Task.CSTS.repository;
-
-import com.Eval_Task.CSTS.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {}
